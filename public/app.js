89ca73330c02cb75558c9943603b6d6ee4c8e499
preisler25 <preisler.andras@gmail.com>
2023-05-29 09:58:14 +0200

    main

--- public/app.js
+++ public/app.js
@@ -8,9 +8,9 @@ let quotationTime = () => {
       return response.json();
     })
     .then(data => {
+      let json_txt = JSON.parse(data)
       // Itt kezelheted a kapott adatokat
-      console.log(data);
-      alert(data)
+      genPopUp(json_txt.today)
       // Például:
       // processResponse(data);
     })
@@ -25,8 +25,21 @@ let genRandom = () =>{
     return `http://localhost:3000/quotation?d=${random}`
 }
 
-let genPopUp = () =>{
-  let element = document.createElement('div')
-  element.classList.add("popUp")
-  
+let delPopUp = () =>{
+  let e = document.getElementById('popUp');
+  e.innerHTML = '';
+  e.classList.remove('active_pop_up');
+}
+
+let genPopUp = (txt) =>{
+  let e = document.getElementById('popUp')
+  e.innerHTML = `<div id="cont">
+  <img id="x_btn" onclick="delPopUp()" src="https://www.freeiconspng.com/thumbs/close-button-png/black-circle-close-button-png-5.png" alt="">
+  <img id="koczka_kep" src="https://media.discordapp.net/attachments/1110164952590856243/1112638199022501910/Screenshot_from_2023-05-29_09-02-25-removebg-preview.png?width=420&height=468" alt="">
+  <div class="popUp_grid_item">
+      <div id="popUp_title">Tanulj ma is Koczkátol!</div>
+      <div id="popUp_quot">"${txt}"</div>
+  </div>
+</div>`;
+  e.classList.add('active_pop_up')
 }
